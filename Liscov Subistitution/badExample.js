3bcfb644e82d2525708793bbd0e784f71cde8c66
Mahmoud Abo-Zeed <mahmoud@Mahmoud.Abozeed>
2021-09-21 23:23:11 +0200

    seperate good and bad example

---
+++ Liscov Subistitution/badExample.js
@@ -0,0 +1,31 @@
+class Bird {
+  fly(speed) {
+    console.log(`Flying at ${speed} km/h`);
+  }
+}
+
+class Eagle extends Bird {
+  dive() {
+    console.log("It dives to a few depths");
+  }
+
+  fly(speed) {
+    console.log(`Soaring through the sky at ${speed}`);
+  }
+}
+
+class Penguin extends Bird {
+  fly() {
+    console.log(new Error("Sorry, I cant fly"));
+  }
+}
+
+const bird = new Bird();
+bird.fly(50); // Flying at 50 km/h
+
+const eagle = new Eagle();
+eagle.dive(); // It dives to a few depths
+eagle.fly(80); // Soaring through the sky at 80
+
+const penguin = new Penguin();
+penguin.fly(); // Error: Sorry, I cant fly
